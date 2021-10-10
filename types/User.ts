7810d0caebdf8dd3adcfa71e1d173c9609b11831
Jason Pierce <jason.pierce.designs@gmail.com>
2021-10-10 17:29:59 -0400

    add typescript and server side call next to add some api routes and tests

---
+++ types/User.ts
@@ -0,0 +1,39 @@
+import { Address } from './Address.tsx';
+import { Company } from './Company.tsx';
+
+export type User = {
+	id: string;
+	name: string;
+	username: string;
+	email: string;
+	address: Address;
+	phone: string;
+	website: string;
+	company: Company;
+};
+
+/**
+ *   {
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+      "street": "Kulas Light",
+      "suite": "Apt. 556",
+      "city": "Gwenborough",
+      "zipcode": "92998-3874",
+      "geo": {
+        "lat": "-37.3159",
+        "lng": "81.1496"
+      }
+    },
+    "phone": "1-770-736-8031 x56442",
+    "website": "hildegard.org",
+    "company": {
+      "name": "Romaguera-Crona",
+      "catchPhrase": "Multi-layered client-server neural-net",
+      "bs": "harness real-time e-markets"
+    }
+  },
+ */
